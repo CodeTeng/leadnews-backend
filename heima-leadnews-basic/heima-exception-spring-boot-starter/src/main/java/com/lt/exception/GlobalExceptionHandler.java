339eecaa5f910779f15b1e782508504dc0e865d7
teng <teng2002823@outlook.com>
2023-01-14 14:33:08 +0800

    add GlobalException_nacos_gateway

---
+++ heima-leadnews-basic/heima-exception-spring-boot-starter/src/main/java/com/lt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.lt.exception;
+
+import com.lt.model.common.enums.AppHttpCodeEnum;
+import com.lt.model.common.vo.ResponseResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @description: 全局异常处理器
+ * @author: ~Teng~
+ * @date: 2023/1/14 11:17
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    /**
+     * 全局异常处理
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseResult globalException(Exception e) {
+        e.printStackTrace();
+        log.error("捕获全局异常: {}", e.getMessage());
+        return ResponseResult.errorResult(AppHttpCodeEnum.SERVER_ERROR);
+    }
+
+    /**
+     * 自定义异常处理
+     */
+    @ExceptionHandler(CustomException.class)
+    public ResponseResult cutsException(CustomException e) {
+        e.printStackTrace();
+        log.error("捕获自定义异常: {}", e.getMessage());
+        return ResponseResult.errorResult(e.getAppHttpCodeEnum());
+    }
+}
---
+++ heima-leadnews-gateways/admin-gateway/src/main/java/com/lt/admin/gateway/AdminGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.lt.admin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: ~Teng~
+ * @date: 2023/1/14 13:38
+ */
+@SpringBootApplication
+public class AdminGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminGatewayApplication.class, args);
+    }
+}
